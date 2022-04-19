da03ef07b5d7620c5c2cc17657ac1bfab16f36c8
Daphne Cheng <daphne.cheng.pink@gmail.com>
2022-04-18 18:22:00 -0700

    Update main.js

--- src/main.js
+++ src/main.js
@@ -5,15 +5,20 @@ console.log("main");
  * 4/18/22
  * Very long
  * 420 hours
- * 
+ * ---
  * Track a high score that persists across scenes and display it in the UI (5)
  * Randomize each spaceship's movement direction at the start of each play (5)
  * Implement parallax scrolling (10)
  * Implement a new timing/scoring mechanism that adds time to the clock for successful hits (20)
  * Redesign the game's artwork, UI, and sound to change its theme/aesthetic (to something other than sci-fi) (60)
- * 
+ * ---
  * collabrated with Alex Xie
+ * ---
  * stop watch icon from https://www.flaticon.com/free-icons/timer
+ * bgm https://www.chosic.com/download-audio/28487/
+ * sfx
+ * all from https://www.mixkit.com
+ * 
  */
 
 
